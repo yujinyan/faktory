b03ac922fe6f99fc1bef99646cac359c235b5f9d
Yu Jinyan <yujinyan@outlook.com>
2021-01-10 15:00:19 +0800

    remove faker dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ publishing {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jeasy:easy-random-core:4.1.0")
-    implementation("com.github.javafaker:javafaker:0.12")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("io.kotest:kotest-assertions-core:4.3.0") // for kotest core jvm assertions
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
